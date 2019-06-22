328ada5ea7fd38e5fbe0c8bae6efe72b01600b38
Jannis R <mail@jannisr.de>
2019-06-22 17:59:51 +0200

    basic benchmark

---
+++ benchmark.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const {Suite} = require('benchmark')
+const {createFlow, flowRunner} = require('.')
+const {add} = require('./ops')
+
+const littleRows = [
+	[0, 0, new Array(100).fill(null).map((_, i) => i)],
+	[1, 1, [50]],
+	[2, 0, new Array(1000).fill(null).map((_, i) => i)]
+]
+
+const chainAdds = (input, n) => {
+	const ops = [input]
+	for (let i = 0; i < n; i++) {
+		const x = [10, 100, 1000][Math.round(Math.random() * 2)]
+		ops.push(add(x))
+	}
+	return createFlow([input], [ops])
+}
+
+const runner5 = flowRunner(chainAdds(littleRows, 5))
+const runner1000 = flowRunner(chainAdds(littleRows, 1000))
+
+new Suite()
+.add('5 adds, ~1k rows', () => {
+	for (const _ of runner5) {}
+})
+.add('1000 adds, ~1k rows', () => {
+	for (const _ of runner1000) {}
+})
+.on('error', (err) => {
+	console.error(err)
+	process.exitCode = 1
+})
+.on('cycle', (e) => {
+	console.log(e.target.toString())
+})
+.run()
